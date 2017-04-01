272940cac469caee923c40b7758ae138e853ae18
alsm <al@hronrad.eu>
2017-04-01 03:38:09 +0100

    Output status to stderr
    This means you can use > to save the output if you want without having to specify an output file.
    Also expect that the url to download will be the first non flag argument passed to the program.
    This also requires that all flags are passed before the url to download.

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 )
 
+var (
+	Status = log.New(os.Stderr, "", 0)
+	Output = os.Stdout
+)
+
 func main() {
-	url := flag.String("url", "", "url")
 	out := flag.String("out", "", "output file")
 
 	flag.Parse()
@@ -19,33 +24,43 @@ func main() {
 	flagset := make(map[string]bool)
 	flag.Visit(func(f *flag.Flag) { flagset[f.Name] = true })
 
-	if flagset["url"] && flagset["out"] {
-		go spinner(100 * time.Millisecond)
-
-		f, err := os.Create(*out)
-		defer f.Close()
-
-		resp, err := http.Get(*url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-
-		n, err := io.Copy(f, resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("\nwrote %d bytes\n", n)
-	} else {
+	// get the first non flag argument
+	target := flag.Arg(0)
+	if _, err := url.Parse(target); err != nil || !strings.HasPrefix(target, "http") || flag.NArg() > 1 {
 		flag.Usage()
+		os.Exit(1)
 	}
+
+	if flagset["out"] {
+		var err error
+		Output, err = os.Create(*out)
+		if err != nil {
+			flag.Usage()
+			Status.Fatalln(err)
+		}
+		defer Output.Close()
+	}
+
+	go spinner(100 * time.Millisecond)
+
+	resp, err := http.Get(target)
+	if err != nil {
+		Status.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	n, err := io.Copy(Output, resp.Body)
+	if err != nil {
+		Status.Fatal(err)
+	}
+
+	Status.Printf("\nwrote %d bytes\n", n)
 }
 
 func spinner(delay time.Duration) {
 	for {
 		for _, r := range `-\|/` {
-			fmt.Printf("\r%c", r)
+			Status.Printf("\r%c", r)
 			time.Sleep(delay)
 		}
 	}
