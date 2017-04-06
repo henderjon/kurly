c46350167f4853be7e926292733544951fdf7da8
David J Peacock <david.j.peacock@gmail.com>
2017-04-06 19:54:17 -0400

    closer to functional for #7

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -12,26 +11,10 @@ import (
 	"github.com/urfave/cli"
 )
 
-var (
-	Status         = log.New(os.Stderr, "", 0)
-	Output         = os.Stdout
-	stdoutRedirect bool
-)
-
-func testForStdoutRedirect() {
-	info, err := os.Stdin.Stat()
-	if err != nil {
-		return
-	}
-	if (info.Mode() & os.ModeCharDevice) != os.ModeCharDevice {
-		stdoutRedirect = true
-	}
-}
-
-var outputFile string
-var target string
-
 func main() {
+	var outputFile string
+	var target string
+
 	app := cli.NewApp()
 	app.Name = "working-title"
 	app.Usage = "[options] URL"
@@ -47,51 +30,44 @@ func main() {
 
 	app.Action = func(c *cli.Context) error {
 		if c.NArg() > 0 {
-			target = c.Args()[0]
-		} else {
-			fmt.Println("URL required")
+			target = c.Args().Get(0)
+		}
+		if outputFile != "" {
+			f, err := os.Create(outputFile)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			resp, err := http.Get(target)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			clHeader := resp.Header.Get("Content-Length")
+			size, err := strconv.ParseInt(clHeader, 10, 64)
+			if err != nil {
+				size = 0
+			}
+
+			progressR := &ioprogress.Reader{
+				Reader: resp.Body,
+				Size:   size,
+				DrawFunc: ioprogress.DrawTerminalf(os.Stderr, func(progress, total int64) string {
+					return fmt.Sprintf(
+						"%s %s",
+						(ioprogress.DrawTextFormatBar(40))(progress, total),
+						ioprogress.DrawTextFormatBytes(progress, total))
+				}),
+			}
+
+			n, err := io.Copy(f, progressR)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("\nwrote %d bytes\n", n)
 		}
 		return nil
 	}
-
-	if outputFile != "" {
-		var err error
-		Output, err = os.Create(outputFile)
-		if err != nil {
-			Status.Fatalln(err)
-		}
-		defer Output.Close()
-	}
-
-	testForStdoutRedirect()
-
-	resp, err := http.Get(target)
-	if err != nil {
-		Status.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	clHeader := resp.Header.Get("Content-Length")
-	size, err := strconv.ParseInt(clHeader, 10, 64)
-	if err != nil {
-		size = 0
-	}
-
-	progressR := &ioprogress.Reader{
-		Reader: resp.Body,
-		Size:   size,
-		DrawFunc: ioprogress.DrawTerminalf(os.Stderr, func(progress, total int64) string {
-			return fmt.Sprintf(
-				"%s %s",
-				(ioprogress.DrawTextFormatBar(40))(progress, total),
-				ioprogress.DrawTextFormatBytes(progress, total))
-		}),
-	}
-
-	n, err := io.Copy(Output, progressR)
-	if err != nil {
-		Status.Fatal(err)
-	}
-
-	Status.Printf("\nwrote %d bytes\n", n)
 }
