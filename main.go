efdc48ff3e4eee61b3bee3efaa374a0132319002
Al S-M <al@hronrad.eu>
2017-04-10 11:21:57 -0400

    Implement -L and --max-redirs (#24)
    -L says to follow 3xx redirects, default is to not
    --max-redirs specifies the maximum number of redirects to follow
    before stopping
    Also I moved the options code out into its own file.

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"net/url"
 	"os"
 	"path"
-	"strconv"
 	"time"
 
 	"github.com/alsm/ioprogress"
@@ -29,67 +28,34 @@ func init() {
 }
 
 func main() {
-	var outputFile *os.File
-	var outputFilename string
-	var remoteName bool
-	var verbose bool
-	var silent bool
 	var target string
-	var maxTime uint
-	var remoteTime bool
+	var outputFile *os.File
+	var opts Options
 
 	app := cli.NewApp()
 	app.Name = "curly"
 	app.Usage = "[options] URL"
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:        "output, o",
-			Usage:       "Filename to name url content to",
-			Destination: &outputFilename,
-		},
-		cli.BoolFlag{
-			Name:        "remote-name, O",
-			Usage:       "Save output to file named with file part of URL",
-			Destination: &remoteName,
-		},
-		cli.BoolFlag{
-			Name:        "v",
-			Usage:       "Verbose output",
-			Destination: &verbose,
-		},
-		cli.BoolFlag{
-			Name:        "silent, s",
-			Usage:       "Mute curly entirely, operation without any output",
-			Destination: &silent,
-		},
-		cli.UintFlag{
-			Name:        "max-time, m",
-			Usage:       "Maximum time to wait for an operation to complete in seconds",
-			Destination: &maxTime,
-		},
-		cli.BoolFlag{
-			Name:        "R",
-			Usage:       "Set the timestamp of the local file to that of the remote file, if available",
-			Destination: &remoteTime,
-		},
-	}
+	opts.getOptions(app)
 
 	app.Action = func(c *cli.Context) error {
 		var remote *url.URL
 		var err error
+
+		client.CheckRedirect = opts.checkRedirect
+
 		if c.NArg() == 0 {
 			cli.ShowAppHelp(c)
 			os.Exit(0)
 		}
 
-		if verbose {
+		if opts.verbose {
 			Status.SetOutput(os.Stderr)
 		}
 
-		if maxTime > 0 {
+		if opts.maxTime > 0 {
 			go func() {
-				<-time.After(time.Duration(maxTime) * time.Second)
-				log.Fatalf("Error: Maximum operation time of %d seconds expired, aborting\n", maxTime)
+				<-time.After(time.Duration(opts.maxTime) * time.Second)
+				log.Fatalf("Error: Maximum operation time of %d seconds expired, aborting\n", opts.maxTime)
 			}()
 
 		}
@@ -99,12 +65,12 @@ func main() {
 			log.Fatalf("Error: %s does not parse correctly as a URL\n", target)
 		}
 
-		if remoteName {
-			outputFilename = path.Base(target)
+		if opts.remoteName {
+			opts.outputFilename = path.Base(target)
 		}
-		if outputFilename != "" {
-			if outputFile, err = os.Create(outputFilename); err != nil {
-				log.Fatalf("Error: Unable to create file '%s' for output\n", outputFilename)
+		if opts.outputFilename != "" {
+			if outputFile, err = os.Create(opts.outputFilename); err != nil {
+				log.Fatalf("Error: Unable to create file '%s' for output\n", opts.outputFilename)
 			}
 		} else {
 			outputFile = os.Stdout
@@ -128,19 +94,16 @@ func main() {
 		}
 		defer resp.Body.Close()
 
-		clHeader := resp.Header.Get("Content-Length")
+		Status.Printf("< %s %s", resp.Proto, resp.Status)
+
 		for k, v := range resp.Header {
 			Status.Println("<", k, v)
 		}
-		size, err := strconv.ParseInt(clHeader, 10, 64)
-		if err != nil {
-			size = 0
-		}
 
-		if !silent {
+		if !opts.silent {
 			progressR := &ioprogress.Reader{
 				Reader: resp.Body,
-				Size:   size,
+				Size:   resp.ContentLength,
 				DrawFunc: ioprogress.DrawTerminalf(os.Stderr, func(progress, total int64) string {
 					return fmt.Sprintf(
 						"%s %s",
@@ -152,19 +115,19 @@ func main() {
 				Status.Fatalf("Error: Failed to copy URL content; %s\n", err)
 			}
 		}
-		if silent {
+		if opts.silent {
 			if _, err = io.Copy(outputFile, resp.Body); err != nil {
 				Status.Fatalf("Error: Failed to copy URL content; %s\n", err)
 			}
 		}
 
-		if outputFilename != "" {
+		if opts.outputFilename != "" {
 			outputFile.Close()
 		}
 
-		if rTime := resp.Header.Get("Last-Modified"); remoteTime && rTime != "" {
+		if rTime := resp.Header.Get("Last-Modified"); opts.remoteTime && rTime != "" {
 			if t, err := time.Parse("Mon, 02 Jan 2006 15:04:05 MST", rTime); err == nil {
-				os.Chtimes(outputFilename, t, t)
+				os.Chtimes(opts.outputFilename, t, t)
 			}
 		}
 		return nil
---
+++ options.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/urfave/cli"
+)
+
+type Options struct {
+	outputFilename string
+	remoteName     bool
+	verbose        bool
+	maxTime        uint
+	remoteTime     bool
+	followRedirect bool
+	maxRedirects   uint
+	redirectsTaken uint
+	silent         bool
+}
+
+func (o *Options) getOptions(app *cli.App) {
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "output, o",
+			Usage:       "Filename to name url content to",
+			Destination: &o.outputFilename,
+		},
+		cli.BoolFlag{
+			Name:        "remote-name, O",
+			Usage:       "Save output to file named with file part of URL",
+			Destination: &o.remoteName,
+		},
+		cli.BoolFlag{
+			Name:        "v",
+			Usage:       "Verbose output",
+			Destination: &o.verbose,
+		},
+		cli.UintFlag{
+			Name:        "max-time, m",
+			Usage:       "Maximum time to wait for an operation to complete in seconds",
+			Destination: &o.maxTime,
+		},
+		cli.BoolFlag{
+			Name:        "R",
+			Usage:       "Set the timestamp of the local file to that of the remote file, if available",
+			Destination: &o.remoteTime,
+		},
+		cli.BoolFlag{
+			Name:        "location, L",
+			Usage:       "Follow 3xx redirects",
+			Destination: &o.followRedirect,
+		},
+		cli.UintFlag{
+			Name:        "max-redirs",
+			Usage:       "Maximum number of 3xx redirects to follow",
+			Destination: &o.maxRedirects,
+			Value:       10,
+		},
+		cli.BoolFlag{
+			Name:        "silent, s",
+			Usage:       "Mute curly entirely, operation without any output",
+			Destination: &o.silent,
+		},
+	}
+}
+
+func (o *Options) checkRedirect(req *http.Request, via []*http.Request) error {
+	o.redirectsTaken++
+
+	if !o.followRedirect || o.redirectsTaken >= o.maxRedirects {
+		return http.ErrUseLastResponse
+	}
+
+	return nil
+}
