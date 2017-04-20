6d30cf301124aae441a07df826dbdc7dd9ceebbc
David J Peacock <david.j.peacock@gmail.com>
2017-04-20 09:56:31 -0400

    Correct TestSetHeaders
    Add check for no header passed to setHeaders
    Set body to nil

--- main_test.go
+++ main_test.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-	"io"
 	"net/http"
 	"strings"
 	"testing"
 )
 
 func TestSetHeaders(t *testing.T) {
-	var body io.Reader
 	header := []string{"User-Agent: Curly/1.0"}
-	req, err := http.NewRequest("GET", "http://url.com/", body)
+	req, err := http.NewRequest("GET", "http://url.com/", nil)
 	if err != nil {
 		panic(err)
 	}
 
 	setHeaders(req, header)
 
-	for _, v := range req.Header {
-		userAgentValue := strings.Join(v, "")
-		if userAgentValue != "Curly/1.0" {
-			t.Errorf("Expected Curly/1.0, but got %g", userAgentValue)
+	if len(req.Header) > 0 {
+		for _, v := range req.Header {
+			userAgentValue := strings.Join(v, "")
+			if userAgentValue != "Curly/1.0" {
+				t.Errorf("Expected Curly/1.0, but got %g", userAgentValue)
+			}
 		}
+	} else {
+		t.Error("No header passed to setHeaders")
 	}
 }
